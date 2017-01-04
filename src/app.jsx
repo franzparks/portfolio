e774324d0d7cc36adc0fa17bc90a9f6c4927f7f6
franzparks <franzparks@gmail.com>
2017-01-04 15:46:14 -0800

    add navbar

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,42 @@ var ReactDOM = require('react-dom');
 
 var Hello = React.createClass({
   render: function() {
-    return <h1 className="red">
-      Welcome To Portfolio!
-    </h1>
+    return (
+        // Navigation
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            {/*Brand and toggle get grouped for better mobile display */}
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="" target="_blank">Francis Phiri</a>
+            </div>
+           {/* Collect the nav links, forms, and other content for toggling */}
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                   <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            {/* /.navbar-collapse */}
+        </div>
+        {/* /.container */}
+    </nav>
+    );
   }
 });
 
