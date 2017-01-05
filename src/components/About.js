b358d83f376e9a9967a5c6bc959f9dac416ebbbe
franzparks <franzparks@gmail.com>
2017-01-05 00:36:34 -0800

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    <h2>About</h2>
-    <p>Am all about the web</p>
-  </div>
+  	<div>
+	    <a  name="about"></a>
+	    <div className="content-section-a">
+		    <div className="bg-overlay2">
+		        <div className="container">
+		            <div className="row">
+		                <div className="col-lg-5 col-sm-6">
+		                    
+		                  <div className="clearfix"></div>
+		                  <h2 className="section-heading">Franz Web Development Studio.</h2>
+		                  <br></br>
+		                    <p className="lead">I'm a <a className="link" target="_blank" 
+		                    href="http://www.freecodecamp.com/franzparks">self taught</a> web developer, based in California,USA.
+		                    <br></br>
+		              		I'm currently part of a software development
+		 					team as an automation software engineer and I am also a regular contributor at Blitz under Udacity
+		 					<br></br> 
+							My passion is to use technology based solutions, to help solve real world challenges.
+							<br></br>
+							Competences:<br></br>
+							Languages and Frameworks:<br></br>
+							Javascript, HTML5, CSS3, Bootstrap3, Angular.js, React.js, Python, Java, Spring Framework.
+							<br></br>
+							Tools & expertise:<br></br>
+							Git, Agile Methodologies.
+							</p>
+		                </div>
+		            	<div className="col-lg-5 col-lg-offset-2 col-sm-6 collapse navbar-collapse">
+		                {/* <img className="img-circle img-responsive" 
+		                    src="https://lh3.googleusercontent.com/-z1lLDOyI7rA/UzJep0xrrDE/AAAAAAAACjg/dKKHx-gJVhg/w326-h220-p/ProfilePhotos" 
+		                    alt="Francis' image"> */}
+		        		</div>
+		        	</div>
+		    	</div>
+		    </div>
+	    </div>	
+    </div>
 );
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav  from './Nav';
 import Header  from './Header';
+import About  from './About';
 // import { version } from '../../package.json';
 
 const App = ({ children }) => (
   <div>
     <Nav />
     <Header />
+    <About />
     <section>
       { children || 'Welcome to Franz Web Studio...More to come!' }
     </section>
