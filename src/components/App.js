42008dc7ee44678d5575a8c6d32ae6560a01beef
franzparks <franzparks@gmail.com>
2017-01-05 00:17:27 -0800

    add nav to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav  from './Nav';
 import Header  from './Header';
 // import { version } from '../../package.json';
 
 const App = ({ children }) => (
   <div>
+    <Nav />
     <Header />
     <section>
       { children || 'Welcome to Franz Web Studio...More to come!' }
