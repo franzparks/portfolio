9b57432d44bf42ae1d54f07004092943ca9d5b19
franzparks <franzparks@gmail.com>
2017-01-05 22:00:05 -0800

    add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,32 @@ import React from 'react';
 
 const Footer = () => (
 
+	<footer>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <ul className="list-inline">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li className="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li className="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#portfolio">Portfolio</a>
+                        </li>
+                        <li className="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                    <p className="copyright text-muted small">Copyright &copy; Franz Web Development Studio 2016. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 );
 
 export default Footer;
