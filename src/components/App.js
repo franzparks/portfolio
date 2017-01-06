616054a6865b44acbc1a0caf97533d4dbbf792d9
franzparks <franzparks@gmail.com>
2017-01-05 21:41:40 -0800

    add portolio component to app

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav  from './Nav';
 import Header  from './Header';
 import About  from './About';
+import Portfolio  from './Portfolio';
 // import { version } from '../../package.json';
 
 const App = ({ children }) => (
@@ -9,6 +10,7 @@ const App = ({ children }) => (
     <Nav />
     <Header />
     <About />
+    <Portfolio />
     <section>
       { children || 'Welcome to Franz Web Studio...More to come!' }
     </section>
