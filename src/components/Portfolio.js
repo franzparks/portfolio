5227609f6e11652e4f12b9c487c8dfc037a1fcac
franzparks <franzparks@gmail.com>
2017-01-05 21:37:52 -0800

    update portolio content

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,147 +1,148 @@
 import React from 'react';
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 
 const Portfolio = () => (
-
-	<a  name="portfolio"></a>
-    <section id="portfolio" className="bg-light-gray bounds">
-        <div classNameName="container">
-            <div className="row">
-                <div className="col-lg-12 text-center">
-                    <h2 className="section-heading">Awesomeness Portfolio.</h2>
-                    <h3 id="below-section" className="section-subheading text-muted">Below you'll find some of my recent work.</h3>
-                </div>
-            </div>
-          {/*  Portfolio img1  */}
-            <div className="row">
-                <div className="col-md-4 col-sm-6 portfolio-item">
-                    <a href="http://codepen.io/franzparks/full/oxZNWm/" target="_blank" className="portfolio-link" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="https://36.media.tumblr.com/a4a06b925ad2ad936ba2ce6c3343c444/tumblr_nwh4epTtMV1ud7rr3o1_1280.jpg" 
-                        className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Twitch.tv API project</h4>
-                        <p id="small-text" className="text-muted">Bootstrap and Angularjs</p>
-                    </div>
-                </div>
-              {/*  End of Portfolio img1  */}
-              
-              {/*  Portfolio img2  */}
-                <div className="col-md-4 col-sm-6 portfolio-item portfolio2">
-                    <a href="http://codepen.io/franzparks/full/jqyqqO/" className="portfolio-link" target="_blank" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="https://40.media.tumblr.com/a73e79ce9844761d7cdc93d202b5cea1/tumblr_nwh4iomduD1ud7rr3o1_1280.jpg" 
-                        className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Wikipedia Viewer</h4>
-                        <p id="small-text" className="text-muted">Bootstrap and AngularJS</p>
-                    </div>
-                </div>
-              {/*  End of Portfolio img2  */}
-              
-              {/*  Portfolio img3  */}
-                <div className="col-md-4 col-sm-6 portfolio-item">
-                    <a href="http://codepen.io/franzparks/full/LNWRZY/" target="_blank" className="portfolio-link" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="https://41.media.tumblr.com/5abea52ea28e9ea20ab995ebd23299a1/tumblr_nwh4iomduD1ud7rr3o2_1280.jpg"
-                         className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Javascript Calculator</h4>
-                        <p id="small-text" className="text-muted">AngularJs and Bootstrap</p>
-                    </div>
-                </div>
-              {/*  End of Portfolio img3  */}
-              
-              {/*  Portfolio img4  */}
-                <div className="col-md-4 col-sm-6 portfolio-item">
-                    <a href="http://codepen.io/franzparks/full/VapMgd/" target="_blank" className="portfolio-link" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="https://40.media.tumblr.com/7512ad23b7e68304a65182988b9c804a/tumblr_nwy76xfEXi1ud7rr3o1_1280.png" 
-                        className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Pomodoro Timer Zipline</h4>
-                        <p id="small-text" className="text-muted">Bootstrap and Angularjs</p>
-                    </div>
-                </div>
-              {/*  End of Portfolio img4  */}
-              
-              {/*  Portfolio img5  */}
-                <div className="col-md-4 col-sm-6 portfolio-item">
-                    <a href="http://codepen.io/franzparks/full/jqVrzx/" target="_blank" className="portfolio-link" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="https://41.media.tumblr.com/59b50efb8f7435fef9f87fd871b90d7a/tumblr_nxhm4k1jI81ud7rr3o1_1280.jpg" 
-                        className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Weather App</h4>
-                        <p id="small-text" className="text-muted">Bootstrap and AngularJS</p>
-                    </div>
-                </div>
-              {/*  End of Portfolio img5  */}
-              
-              {/*  Portfolio img6  */}
-                <div className="col-md-4 col-sm-6 portfolio-item">
-                    <a href="http://codepen.io/franzparks/full/GZmaeG/" target="_blank" className="portfolio-link" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="https://36.media.tumblr.com/030b4966650a0d1dac0bbcca6fe572e8/tumblr_nxhm7qz6Y91ud7rr3o1_1280.jpg" 
-                        className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Tic Tac Toe Game</h4>
-                        <p id="small-text" className="text-muted">AngularJs and Bootstrap</p>
-                    </div>
-                </div>
-              {/*  End of Portfolio img6  */}
-              {/* Portfolio img7 */}
-               <div className="row">
-                <div className="col-md-4 col-sm-6 portfolio-item">
-                    <a href="http://codepen.io/franzparks/full/vGJvJM/" target="_blank" className="portfolio-link" data-toggle="modal">
-                        <div className="portfolio-hover">
-                            <div className="portfolio-hover-content">
-                                <i className="fa fa-fire fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="http://www.geekalerts.com/u/classNameic-Simon-Game.jpg" className="img-responsive" alt=""></img>
-                    </a>
-                    <div className="portfolio-caption">
-                        <h4>Simon Game</h4>
-                        <p id="small-text" className="text-muted">Bootstrap and Angularjs</p>
-                    </div>
-                </div>
-              {/* End of Portfolio img7 */}
-              
-            </div>
-        </div>
-    </section>
-
+	<div>
+		<a  name="portfolio"></a>
+	    <section id="portfolio" className="bg-light-gray bounds">
+	        <div classNameName="container">
+	            <div className="row">
+	                <div className="col-lg-12 text-center">
+	                    <h2 className="section-heading">Awesomeness Portfolio.</h2>
+	                    <h3 id="below-section" className="section-subheading text-muted">Below you'll find some of my recent work.</h3>
+	                </div>
+	            </div>
+	          {/*  Portfolio img1  */}
+	            <div className="row">
+	                <div className="col-md-4 col-sm-6 portfolio-item">
+	                    <a href="http://codepen.io/franzparks/full/oxZNWm/" target="_blank" className="portfolio-link" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="https://36.media.tumblr.com/a4a06b925ad2ad936ba2ce6c3343c444/tumblr_nwh4epTtMV1ud7rr3o1_1280.jpg" 
+	                        className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Twitch.tv API project</h4>
+	                        <p id="small-text" className="text-muted">Bootstrap and Angularjs</p>
+	                    </div>
+	                </div>
+	              {/*  End of Portfolio img1  */}
+	              
+	              {/*  Portfolio img2  */}
+	                <div className="col-md-4 col-sm-6 portfolio-item portfolio2">
+	                    <a href="http://codepen.io/franzparks/full/jqyqqO/" className="portfolio-link" target="_blank" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="https://40.media.tumblr.com/a73e79ce9844761d7cdc93d202b5cea1/tumblr_nwh4iomduD1ud7rr3o1_1280.jpg" 
+	                        className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Wikipedia Viewer</h4>
+	                        <p id="small-text" className="text-muted">Bootstrap and AngularJS</p>
+	                    </div>
+	                </div>
+	              {/*  End of Portfolio img2  */}
+	              
+	              {/*  Portfolio img3  */}
+	                <div className="col-md-4 col-sm-6 portfolio-item">
+	                    <a href="http://codepen.io/franzparks/full/LNWRZY/" target="_blank" className="portfolio-link" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="https://41.media.tumblr.com/5abea52ea28e9ea20ab995ebd23299a1/tumblr_nwh4iomduD1ud7rr3o2_1280.jpg"
+	                         className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Javascript Calculator</h4>
+	                        <p id="small-text" className="text-muted">AngularJs and Bootstrap</p>
+	                    </div>
+	                </div>
+	              {/*  End of Portfolio img3  */}
+	              
+	              {/*  Portfolio img4  */}
+	                <div className="col-md-4 col-sm-6 portfolio-item">
+	                    <a href="http://codepen.io/franzparks/full/VapMgd/" target="_blank" className="portfolio-link" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="https://40.media.tumblr.com/7512ad23b7e68304a65182988b9c804a/tumblr_nwy76xfEXi1ud7rr3o1_1280.png" 
+	                        className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Pomodoro Timer Zipline</h4>
+	                        <p id="small-text" className="text-muted">Bootstrap and Angularjs</p>
+	                    </div>
+	                </div>
+	              {/*  End of Portfolio img4  */}
+	              
+	              {/*  Portfolio img5  */}
+	                <div className="col-md-4 col-sm-6 portfolio-item">
+	                    <a href="http://codepen.io/franzparks/full/jqVrzx/" target="_blank" className="portfolio-link" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="https://41.media.tumblr.com/59b50efb8f7435fef9f87fd871b90d7a/tumblr_nxhm4k1jI81ud7rr3o1_1280.jpg" 
+	                        className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Weather App</h4>
+	                        <p id="small-text" className="text-muted">Bootstrap and AngularJS</p>
+	                    </div>
+	                </div>
+	              {/*  End of Portfolio img5  */}
+	              
+	              {/*  Portfolio img6  */}
+	                <div className="col-md-4 col-sm-6 portfolio-item">
+	                    <a href="http://codepen.io/franzparks/full/GZmaeG/" target="_blank" className="portfolio-link" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="https://36.media.tumblr.com/030b4966650a0d1dac0bbcca6fe572e8/tumblr_nxhm7qz6Y91ud7rr3o1_1280.jpg" 
+	                        className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Tic Tac Toe Game</h4>
+	                        <p id="small-text" className="text-muted">AngularJs and Bootstrap</p>
+	                    </div>
+	                </div>
+	              {/*  End of Portfolio img6  */}
+	              {/* Portfolio img7 */}
+	               <div className="row">
+	                <div className="col-md-4 col-sm-6 portfolio-item">
+	                    <a href="http://codepen.io/franzparks/full/vGJvJM/" target="_blank" className="portfolio-link" data-toggle="modal">
+	                        <div className="portfolio-hover">
+	                            <div className="portfolio-hover-content">
+	                                <i className="fa fa-fire fa-3x"></i>
+	                            </div>
+	                        </div>
+	                        <img src="http://www.geekalerts.com/u/classNameic-Simon-Game.jpg" className="img-responsive" alt=""></img>
+	                    </a>
+	                    <div className="portfolio-caption">
+	                        <h4>Simon Game</h4>
+	                        <p id="small-text" className="text-muted">Bootstrap and Angularjs</p>
+	                    </div>
+	                </div>
+	              	{/* End of Portfolio img7 */}
+	              
+	            </div>
+	        </div>
+	    </div>
+	    </section>
+    </div>
 );
 
 export default Portfolio;
