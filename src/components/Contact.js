6fca9ae0d32a00ad623bd0960eebc39eae5d0b76
franzparks <franzparks@gmail.com>
2017-01-05 21:47:58 -0800

    add contact content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,42 @@ import { React } from 'react';
 
 const Contact = () => (
 
+	<a  name="contact"></a>
+    <div className="banner">
+      <div className="bg-overlay4">
+        <div className="container">         
+            <div className="row">
+                <div className="col-lg-6">
+                  <h2 className="connect">Connect with Franz Web Dev Studio: franzparks@gmail.com</h2>
+                </div>
+              </br>
+      
+                <div className="col-lg-6">
+                    <ul className="list-inline banner-social-buttons">
+                        <li>
+                                <a href="https://twitter.com/franzparks" target="_blank" className="btn btn-default btn-lg">
+                                <i className="fa fa-twitter fa-fw"></i> <span className="network-name">Twitter</span></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/franzparks" target="_blank" className="btn btn-default btn-lg ">
+                                <i className="fa fa-github fa-fw"></i> <span className="network-name">Github</span></a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/franzparks" target="_blank" className="btn btn-default btn-lg">
+                                <i className="fa fa-linkedin fa-fw"></i> <span className="network-name">Linkedin</span></a>
+                            </li>
+                            <li>
+                                <a href="http://www.freecodecamp.com/franzparks" target="_blank" className="btn btn-default btn-lg">
+                                <i className="fa fa-fire fa-fw"></i> <span className="network-name">freeCodeCamp</span></a>
+                            </li>
+                      
+                    </ul>
+                </div>
+        </div>
+   
+    </div>
+</div>
+</div>
 
 );
 
